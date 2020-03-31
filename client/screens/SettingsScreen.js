5f8d74019ea07e313c87ace337b1a379a5369243
Tilli Buchanan <tbuchanan@overstock.com>
2020-03-30 19:57:48 -0600

    - Adding Custom font (Werewolf.ttf) - Wip with changing the icons on SettingsScreen to the icons provided by the custom font Werewolf (as icons were already displaying on that screen) - Adding Werewolf font to async-load at App start.

--- client/screens/SettingsScreen.js
+++ client/screens/SettingsScreen.js
@@ -1,28 +1,34 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Font } from 'react-native';
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons/Icons';
+import icoMoonSelection from '../selection.json';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import Colors from "../constants/Colors";
 
+const MoonIcon = createIconSetFromIcoMoon(icoMoonSelection);
+
 export default function SettingsScreen() {
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      {/* <View>
+        <MoonIcon name="werewolf-token" size={80} color="#bf1313" />
+      </View> */}
       <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
+        icon="werewolf-token"
+        label="Werewolf"
         onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
       />
 
       <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
+        icon="villager-token"
+        label="Villager"
         onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
       />
 
       <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
+        icon="seer-token"
+        label="Seer"
         onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
         isLastOption
       />
@@ -30,12 +36,15 @@ export default function SettingsScreen() {
   );
 }
 
+
+
 function OptionButton({ icon, label, onPress, isLastOption }) {
+
   return (
     <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
       <View style={{ flexDirection: 'row' }}>
         <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+          <MoonIcon name={icon} size={22} color="rgba(0,0,0,0.35)" />
         </View>
         <View style={styles.optionTextContainer}>
           <Text style={styles.optionText}>{label}</Text>
