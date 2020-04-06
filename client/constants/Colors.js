a1e0f9c8521b5fd49e48c4cce4e55729950eaf63
Jared Buchanan <jcbuchanan@sofi.org>
2020-04-05 20:48:08 -0600

    - tryna figure out color themes swappage...

--- client/constants/Colors.js
+++ client/constants/Colors.js
@@ -1,11 +1,38 @@
-const tintColor = '#2f95dc';
+const activeText = '#5A82E7';
+const inactiveText = '#4E48C8';
+const inputText = '#D8D7D6';
+const buttonBackground = '#5D4D78';
+const buttonSelectedBorder = '#D8D7D6';
+const buttonSelected = '#7154A1';
+const buttonUnselected = '#45249C';
+const buttonText = '#999693';
 
 export default {
   tintColor,
-  // noticeText: '#fff',
+  activeText,
+  inactiveText,
+  inputText,
+  buttonBackground,
+  buttonSelectedBorder,
+  buttonSelected,
+  buttonUnselected,
+  buttonText,
 };
 
 export const NightTheme = {
+  darkBlue: '#261C36',
+  trueBlue: '#4E48C8',
+  activeText: '#5A82E7',
+  inactiveText: '#4E48C8',
+  inputText: '#D8D7D6',
+  buttonBackground: '#5D4D78',
+  buttonSelectedBorder: '#D8D7D6',
+  buttonSelected: '#7154A1',
+  buttonUnselected: '#45249C',
+  buttonText: '#999693',
+};
+
+export const DayTheme = {
   darkBlue: '#261C36',
   trueBlue: '#4E48C8',
   activeText: '#5A82E7',
